2d22dd3dafd9bed76e957c1cc14850bcad51a372
Caio Bomfim <caaiobomfim@gmail.com>
2025-02-15 21:05:10 -0300

    fix: update function name

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	result := Sum(15, 15)
+	result := sum(15, 15)
 
 	if result != 30 {
 		t.Errorf("RESULT IS INVALID: EXPECTED RESULT %d.: %d ", result, 30)
